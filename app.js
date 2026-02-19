79d297dddee4894a93bc472e23b8d43ca54143e0
naveen305 <naveen305@gmail.com>
2026-02-19 14:10:55 +0530

    chore: dependecy packages

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
-});
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(port, (err) => {
+        if (err) {
+            return console.log('Something bad happened', err);
+        }
+        console.log(`Server is listening on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1); // Exit process if DB fails
+  })
 
 
 
